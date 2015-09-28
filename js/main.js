a8ef4d80f8b7355cd2f77cd283a3d66d4fc47ee1
Paulo Brumatti <paulo.brumatti@dp6.com.br>
2015-09-28 10:15:05 -0300

    Automação e mudança de nomenclatura

--- js/main.js
+++ js/main.js
@@ -1,7 +1,77 @@
-(function() {
+var data = {
+	"MATERIAS": [{
+		"PROFESSOR": "OSWALDO ORTIZ FERNANDES JUNIOR",
+		"MATERIA": "INTELIGÊNCIA ARTIFICIAL E COMPUTACIONAL",
+		"FALTAS": 12,
+		"LIMITE_FALTAS": 20,
+		"P1": 5.6,
+		"P2": -1,
+		"P3": -1,
+		"MEDIA": -1
+	}, {
+		"PROFESSOR": "RICARDO RESENTE DE MENDONÇA",
+		"MATERIA": "COMPUTAÇÃO GRÁFICA E PROCESSAMENTO DE IMAGEM",
+		"FALTAS": 0,
+		"LIMITE_FALTAS": 0,
+		"P1": 6,
+		"P2": 4,
+		"P3": -1,
+		"MEDIA": 5
+	}, {
+		"PROFESSOR": "OSWALDO ORTIZ FERNANDES JUNIOR",
+		"MATERIA": "INTELIGÊNCIA ARTIFICIAL E COMPUTACIONAL",
+		"FALTAS": 12,
+		"LIMITE_FALTAS": 20,
+		"P1": 5.6,
+		"P2": -1,
+		"P3": -1,
+		"MEDIA": -1
+	}, {
+		"PROFESSOR": "RICARDO RESENTE DE MENDONÇA",
+		"MATERIA": "COMPUTAÇÃO GRÁFICA E PROCESSAMENTO DE IMAGEM",
+		"FALTAS": 0,
+		"LIMITE_FALTAS": 0,
+		"P1": 6,
+		"P2": 4,
+		"P3": -1,
+		"MEDIA": 5
+	}],
+	"AACC": {
+		"RECENTES": [{
+			"NOME_AACC": "Cine USCS",
+			"HORAS_AACC": 15
+		}, {
+			"NOME_AACC": "Os Vingadores",
+			"HORAS_AACC": 4
+		}],
+		"HORAS_REALIZADAS": 19,
+		"HORAS_NECESSARIAS": 190
+	},
+	"SOLICITACOES": [{
+		'ID_SOLICITACAO': 'mudanca-de-sala',
+		'NOME_SOLICITACAO': 'Mudança de Sala'
+	}, {
+		'ID_SOLICITACAO': 'mudanca-de-periodo',
+		'NOME_SOLICITACAO': 'Mudança de Período'
+	}, {
+		'ID_SOLICITACAO': 'mudanca-de-curso',
+		'NOME_SOLICITACAO': 'Mudança de Curso'
+	}, {
+		'ID_SOLICITACAO': 'dispensa-de-disciplina',
+		'NOME_SOLICITACAO': 'Dispensa de Disciplina'
+	}]
+};
+
+(function () {
 	var TARCI = {};
 
 	TARCI.sections = jQuery('.state');
+	TARCI.templates = {
+		'materia': jQuery('#template-materia').html(),
+		'aaccRecentes': jQuery('#template-aacc-recentes').html(),
+		'aaccVisaoGeral': jQuery('#template-aacc-visaogeral').html(),
+		'solicitacoes': jQuery('#template-solicitacoes').html(),
+	};
 	TARCI.init = function init() {
 
 		// TODO - Migrar para config externo em YML
@@ -47,8 +117,8 @@
 			});
 		});*/
 		TARCI.util.loadingScreen(true);
-		setTimeout(function() {
-			TARCI.stateDashboard();
+		setTimeout(function () {
+			TARCI.stateDashboard(data);
 			TARCI.util.loadingScreen(false);
 		}, 2000);
 	};
@@ -58,13 +128,21 @@
 		document.location.reload();
 	};
 
-	TARCI.stateDashboard = function stateDashboard () {
+	TARCI.stateDashboard = function stateDashboard(data) {
+		if (typeof data !== 'object')
+			throw new Error('[TARCI.stateDashboard] Dados recebidos incorretos');
+
+		TARCI.util.parseData('materias', data.MATERIAS);
+		TARCI.util.parseData('aaccVisaoGeral', data.AACC);
+		TARCI.util.parseData('aaccRecentes', data.AACC.RECENTES);
+		TARCI.util.parseData('solicitacoes', data.SOLICITACOES);
+
 		toggleSection('dashboard');
 	};
 
 	function toggleSection(section) {
 		document.body.className = section;
-		TARCI.sections.each(function(i, el) {
+		TARCI.sections.each(function (i, el) {
 			if (el.id === 'state-' + section)
 				el.className.split(' ').indexOf('show') === -1 && el.classList.add('show');
 			else
--- js/util.js
+++ js/util.js
@@ -1,5 +1,6 @@
-(function() {
+(function () {
 	var util = TARCI.util = {};
+	var loadingScreenElement = jQuery('#loading-screen');
 
 	util.find = function find(selector, scope) {
 		if (!scope) scope = document;
@@ -12,13 +13,13 @@
 
 	util.ajaxTimeout = function ajaxTimeout(url, data, callback, timeout) {
 		util.loadingScreen(true);
-		var ajaxPromise = jQuery.getJSON(url, data, function() {
+		var ajaxPromise = jQuery.getJSON(url, data, function () {
 			util.loadingScreen(false);
 			clearTimeout(ajaxLife);
 			callback.apply(this, arguments);
 		});
 
-		var ajaxLife = setTimeout(function() {
+		var ajaxLife = setTimeout(function () {
 			util.loadingScreen(false);
 			ajaxPromise.abort();
 			TARCI.util.message({
@@ -28,13 +29,133 @@
 		}, timeout);
 	};
 
-	var loadingScreenElement = jQuery('#loading-screen');
-
 	util.loadingScreen = function loadingScreen(show) {
 		loadingScreenElement.toggleClass('loading-show', show)
 	};
 
 	util.message = function message(obj) {
 		alert(obj.type + '\n\n' + obj.message);
+	};
+
+	util.template = function template(tmpl, obj) {
+		if (typeof tmpl !== 'string')
+			throw new Error('[util.template] Template inválido: ' + tmpl);
+		if (typeof obj !== 'object')
+			throw new Error('[util.template] Parametros inválidos: ' + tmpl);
+		for (var key in obj) {
+			tmpl = tmpl.replace('{{' + key + '}}', obj[key]);
+		}
+
+		return tmpl;
+	};
+
+	util.validateParams = function validateParams(obj, params) {
+		if (typeof obj !== 'object')
+			throw new Error('[util.validateParams] Objeto a ser validado invalido: ' + obj);
+		if (typeof params !== 'object')
+			throw new Error('[util.validateParams] Parametros a serem validados invalido: ' + obj);
+
+		for (var key in params)
+			if (typeof obj[key] !== params[key])
+				return false;
+		return true;
+	};
+
+	util.validate = function validate(type, obj) {
+		if (!type || typeof type !== 'string')
+			throw new Error('[util.validate] Tipo de validação inválida: ' + type);
+		if (!obj || typeof obj !== 'object')
+			throw new Error('[util.validate] Tipo de objeto inválido: ' + obj);
+		if (typeof util.validate[type] !== 'object')
+			throw new Error('[util.validate] Não há validação para este tipo de dados:' + type);
+
+		return util.validateParams(obj, util.validate[type]);
+	};
+
+	util.validate.materia = {
+		'PROFESSOR': 'string',
+		'MATERIA': 'string',
+		'FALTAS': 'number',
+		'LIMITE_FALTAS': 'number',
+		'P1': 'number',
+		'P2': 'number',
+		'P3': 'number',
+		'MEDIA': 'number'
+	};
+
+	util.validate.aaccVisaoGeral = {
+		'HORAS_REALIZADAS': 'number',
+		'HORAS_NECESSARIAS': 'number'
+	};
+
+	util.validate.aaccRecentes = {
+		'NOME_AACC': 'string',
+		'HORAS_AACC': 'number'
+	}
+
+	util.validate.solicitacoes = {
+		'ID_SOLICITACAO': 'string',
+		'NOME_SOLICITACAO': 'string'
+	};
+
+	util.parseData = function parseData(type, data) {
+		if (typeof type !== 'string' || !type)
+			throw new Error('[util.parseData] O tipo de dados é inválido: ' + type);
+		if (typeof data !== 'object' || !data)
+			throw new Error('[util.parseData] Os dados não estão no formato correto: ' + data);
+		if (typeof util.parseData[type] !== 'function')
+			throw new Error('[util.parseData] Não existe validação para o tipo recebido: ' + type)
+
+		return util.parseData[type](data);
+	};
+
+	util.parseData.materias = function (data) {
+		if (!data || !data.length)
+			throw new Error('[util.parseData.materias] Os dados devem estar contidos em um array: ' + data);
+
+		data.forEach(function(materia) {
+			util.validate('materia', materia);
+			materia.FALTAS = materia.FALTAS === -1 ? '-' : materia.FALTAS;
+			materia.LIMITE_FALTAS = materia.LIMITE_FALTAS === -1 ? '-' : materia.LIMITE_FALTAS;
+			materia.P1 = materia.P1 === -1 ? '-' : materia.P1;
+			materia.P2 = materia.P2 === -1 ? '-' : materia.P2;
+			materia.P3 = materia.P3 === -1 ? '-' : materia.P3;
+			materia.MEDIA = materia.MEDIA === -1 ? '-' : materia.MEDIA;
+			var structure = util.template(TARCI.templates.materia, materia);
+			jQuery('.card-materias-content').append(structure);
+		});
+	};
+
+	util.parseData.aaccVisaoGeral = function (data) {
+		if (!data || typeof data !== 'object')
+			throw new Error('[util.parseData.aaccVisaoGeral] Os dados devem estar contidos em um objeto: ' + data);
+
+		util.validate('aaccVisaoGeral', data);
+		data.PORCENTAGEM_REALIZADA = 100 * (data.HORAS_REALIZADAS / data.HORAS_NECESSARIAS);
+		data.PORCENTAGEM_NECESSARIA = 100 * (1 - (data.HORAS_REALIZADAS / data.HORAS_NECESSARIAS));
+		var structure = util.template(TARCI.templates.aaccVisaoGeral, data);
+		jQuery('.card-aacc-content').append(structure);
+	};
+
+	util.parseData.aaccRecentes = function (data) {
+		if (!data || !data.length)
+			throw new Error('[util.parseData.aaccRecentes] Os dados devem estar contidos em um array: ' + data);
+		
+		data.forEach(function(recente){	
+			util.validate('aaccRecentes', recente);
+			var structure = util.template(TARCI.templates.aaccRecentes, recente);
+			jQuery('.card-aacc-recentes-tabela').append(structure);
+		});
+	};
+
+	util.parseData.solicitacoes = function (data) {
+		if (!data || !data.length)
+			throw new Error('[util.parseData.solicitacoes] Os dados devem estar contidos em um array: ' + data);
+		
+		data.forEach(function(solicitacao){	
+			util.validate('solicitacoes', solicitacao);
+			var structure = util.template(TARCI.templates.solicitacoes, solicitacao);
+			jQuery('.card-solicitacoes-lista').append(structure);
+		});
 	};
 }());
